1b40ce4b2b5a2a55bd10acb45016e0e63956c2d6
yondaniot <82291260+yondaniot@users.noreply.github.com>
2021-04-11 12:13:08 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,11 @@
 // @ts-ignore
 firebase.initializeApp({
+   
    apiKey: "AIzaSyAix7TfnisH4wfXoh4W13F1NI3dBKsYnrY",
     authDomain: "yondaniot-138a2.firebaseapp.com",
     projectId: "yondaniot-138a2",
     storageBucket: "yondaniot-138a2.appspot.com",
     messagingSenderId: "937217822499",
     appId: "1:937217822499:web:089ce498be2ea4660bc696"
+  };
 });
